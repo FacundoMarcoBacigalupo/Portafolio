2b4a7cdc290c56c0c71b94b0e9f94b901216ed22
Facundo Marco Bacigalupo <marcofacundolucas@gmail.com>
2023-11-21 18:21:52 -0300

    Update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import gitHub from "../../img/githubFooter.png"
 import linkedin from "../../img/linkedinFooter.png"
 import gmail from "../../img/gmail.png"
+import whatsApp from "../../img/whatsapp.png"
 import "./Contact.css"
 
 
@@ -12,16 +13,21 @@ const Contact = () => {
 
             <section id='grillaContact'>
                 <div id='contact1'>
+                    <img className='imgRRSS' src={whatsApp} alt="WhatsApp" />
+                    <a className='stylesA' href='https://wa.me/+5401127398858' target="_blank" rel="noreferrer">WhatsApp</a>
+                </div>
+
+                <div id='contact2'>
                     <img className='imgRRSS' src={gitHub} alt="GitHub" />
                     <a className='stylesA' href='https://github.com/FacundoMarcoBacigalupo' target="_blank" rel="noreferrer">GitHub</a>
                 </div>
 
-                <div id='contact2'>
+                <div id='contact3'>
                     <img className='imgRRSS' src={linkedin} alt="LinkeInd" />
                     <a className='stylesA' href='https://www.linkedin.com/in/facundolucasmarco/' target="_blank" rel="noreferrer">LinkeInd</a>
                 </div>
 
-                <div id='contact3'>
+                <div id='contact4'>
                     <img className='imgRRSS' src={gmail} alt="Gmail" />
                     <a className='stylesA' href='https://mail.google.com/mail/u/1/#sent' target="_blank" rel="noreferrer">Gmail</a>
                 </div>
