104eb36c4e99386b7e035b7c4a4345cb80b7da46
unknown <marcofacundolucas@gmail.com>
2024-01-15 11:23:30 -0300

    Update of logos

--- src/components/Knows/Knows.jsx
+++ src/components/Knows/Knows.jsx
@@ -9,6 +9,8 @@ import javascript from '../../img/javascript.png'
 import reactJs from '../../img/reactJs.png'
 import responsive from '../../img/responsive.png'
 import sass from '../../img/sass.png'
+import mongodb from '../../img/mongodb.png'
+import nodejs from '../../img/nodejs.png'
 import "./Knows.css"
 
 
@@ -19,53 +21,63 @@ const Knows = () => {
 
             <section id='grillaContainer'>
                 <div id='img1'>
-                    <img className='styleImg' src={VScode} alt="Conocimiento 1" />
+                    <img className='styleImg' src={VScode} alt="Know 1" />
                     <p>Visual Studio Code</p>
                 </div>
 
                 <div id='img2'>
-                    <img className='styleImg' src={bootstrap} alt="Conocimiento 2" />
+                    <img className='styleImg' src={bootstrap} alt="Know 2" />
                     <p>Bootstrap</p>
                 </div>
 
                 <div id='img3'>
-                    <img className='styleImg' src={css} alt="Conocimiento 3" />
+                    <img className='styleImg' src={css} alt="Know 3" />
                     <p>Css</p>
                 </div>
 
                 <div id='img4'>
-                    <img className='styleImg' src={sass} alt="Conocimiento 4" />
+                    <img className='styleImg' src={sass} alt="Know 4" />
                     <p>Sass</p>
                 </div>
 
                 <div id='img5'>
-                    <img className='styleImg' src={github} alt="Conocimiento 5" />
+                    <img className='styleImg' src={github} alt="Know 5" />
                     <p>GitHub</p>
                 </div>
 
                 <div id='img6'>
-                    <img className='styleImg' src={reactJs} alt="Conocimiento 6" />
-                    <p>React Js</p>
+                    <img className='styleImg' src={reactJs} alt="Know 6" />
+                    <p>React.   Js</p>
                 </div>
 
                 <div id='img7'>
-                    <img className='styleImg' src={javascript} alt="Conocimiento 7" />
+                    <img className='styleImg' src={javascript} alt="Know 7" />
                     <p>JavaScript</p>
                 </div>
 
                 <div id='img8'>
-                    <img className='styleImg' src={html} alt="Conocimiento 8" />
+                    <img className='styleImg' src={html} alt="Know 8" />
                     <p>html</p>
                 </div>
 
                 <div id='img9'>
-                    <img className='styleImg' src={responsive} alt="Conocimiento 9" />
+                    <img className='styleImg' src={responsive} alt="Know 9" />
                     <p>Diseño Responvisve</p>
                 </div>
 
                 <div id='img10'>
-                    <img className='styleImg' src={git} alt="Conocimiento 10" />
+                    <img className='styleImg' src={git} alt="Know 10" />
                     <p>Git</p>
+                </div>
+
+                <div id='img11'>
+                    <img className='styleImg' src={mongodb} alt="Know 11" />
+                    <p>MongoDB</p>
+                </div>
+
+                <div id='img12'>
+                    <img className='styleImg' src={nodejs} alt="Know 12" />
+                    <p>Node.js</p>
                 </div>
             </section>
         </article>
